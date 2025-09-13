7743d679dc2e7b3398e037624c701f61959f31e4
avtss <avetiszadikyann@mail.ru>
2025-09-13 22:55:14 +0300

    Lab2.1

---
+++ Lab2/HSVFromCamera.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        cv2.imshow('Camera - Original', frame)
+        cv2.imshow('Camera - HSV', hsv)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+
