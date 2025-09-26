fc010f889f00473737467702fb3c1ede7e934743
avtss <avetiszadikyann@mail.ru>
2025-09-26 21:12:59 +0300

    fix

--- Lab2/HSVFromCamera.py
+++ Lab2/HSVFromCamera.py
@@ -1,28 +1,25 @@
 import cv2
 
 
-def main():
-    cap = cv2.VideoCapture(0)
-    cap.set(3, 640)
-    cap.set(4, 480)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        cv2.imshow('Camera - Original', frame)
-        cv2.imshow('Camera - HSV', hsv)
+    cv2.imshow('Camera - Original', frame)
+    cv2.imshow('Camera - HSV', hsv)
 
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
 
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 
-if __name__ == "__main__":
-    main()
 
