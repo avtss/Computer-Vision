13edc6d1d6f73c0ed69c7210350d70b9660a6781
avtss <avetiszadikyann@mail.ru>
2025-10-03 10:00:11 +0300

    fix

--- Lab1/AWPScopeOnCamera.py
+++ Lab1/AWPScopeOnCamera.py
@@ -11,17 +11,17 @@ while True:
         break
 
     h, w = frame.shape[:2]
-    x0, y0 = w // 2, h // 2
+    x, y = w // 2, h // 2
     r = 200
 
     Y, X = np.ogrid[:h, :w]
-    mask = (X- x0) ** 2 + (Y - y0) ** 2 <= r ** 2
+    mask = (X- x) ** 2 + (Y - y) ** 2 >= r ** 2
 
-    frame[~mask] = 0
-    cv2.circle(frame, (x0, y0), r, (0, 0, 0), 2)
+    frame[mask] = 0
+    cv2.circle(frame, (x, y), r, (0, 0, 0), 2)
 
-    cv2.line(frame, (0, y0), (w, y0), (0, 0, 0), 2)
-    cv2.line(frame, (x0, 0), (x0, h), (0, 0, 0), 2)
+    cv2.line(frame, (0, y), (w, y), (0, 0, 0), 2)
+    cv2.line(frame, (x, 0), (x, h), (0, 0, 0), 2)
 
     cv2.imshow("Sniper Scope", frame)
 
